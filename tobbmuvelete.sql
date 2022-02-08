4bb22869a31d227e31de37078258669b0a4da12a
gostonnn <agoston.vasarhelyi@gmail.com>
2022-02-08 11:31:52 +0100

    aaa

--- tobbmuvelete.sql
+++ tobbmuvelete.sql
@@ -1,3 +1,8 @@
 DELIMITER $$
 CREATE PROCEDURE more(OUT less INT, OUT aver DOUBLE, OUT top
+BEGIN
+	SELECT MIN( fizetes ) INTO less FROM dolgozok;
+	SELECT AVG( fizetes ) INTO aver FROM dolgozok;
+	SELECT MAX( fizetes ) INTO top FROM dolgozok;
+END $$
 DELIMITER ;
