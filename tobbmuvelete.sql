0aa02b2d9ae3371ef2f80fdd0ca35e40b41c93c7
gostonnn <agoston.vasarhelyi@gmail.com>
2022-02-08 11:35:16 +0100

    a

--- tobbmuvelete.sql
+++ tobbmuvelete.sql
@@ -1,8 +1,12 @@
 DELIMITER $$
-CREATE PROCEDURE more(OUT less INT, OUT aver DOUBLE, OUT top
+CREATE PROCEDURE more(OUT less INT, OUT aver DOUBLE, OUT top INT)
 BEGIN
 	SELECT MIN( fizetes ) INTO less FROM dolgozok;
 	SELECT AVG( fizetes ) INTO aver FROM dolgozok;
 	SELECT MAX( fizetes ) INTO top FROM dolgozok;
 END $$
 DELIMITER ;
+
+call more(@min, @avg, @max);
+SELECT @min as legkisebb, @avg as átlag, @max as legtöbb
+
