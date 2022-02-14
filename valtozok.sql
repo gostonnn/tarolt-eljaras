08ebe9309960390dd045cf4e663460660a501bde
gostonnn <agoston.vasarhelyi@gmail.com>
2022-02-14 12:08:20 +0100

    a

--- valtozok.sql
+++ valtozok.sql
@@ -69,3 +69,64 @@ SELECT @price as ar;
 
 6.
 
+FOS
+
+DELIMITER $$
+CREATE PROCEDURE vevonevlistaA()
+BEGIN
+DECLARE finished INT DEFAULT 0;
+DECLARE name_list VARCHAR(500) DEFAULT "";
+DECLARE user_name VARCHAR(50) DEFAULT "";
+DECLARE user_data CURSOR FOR SELECT vevonev FROM vevok;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+OPEN user_data;
+get_user_name: LOOP
+	FETCH user_data INTO  user_name;
+	IF finished = 1 THEN
+		LEAVE get_user_name;
+	END IF;
+	SET name_list = CONCAT ( name_list, ", ", UPPER( user_name ));
+END LOOP get_user_name;
+CLOSE user_data;
+END $$
+DELIMITER ;
+
+7.
+
+DELIMITER $$
+CREATE PROCEDURE ujRekord( IN futarnev VARCHAR(50), IN tel INT)
+BEGIN
+INSERT INTO futarok ( futarnev, tel) VALUES
+					( futarnev, tel);
+END $$
+DELIMITER ;
+
+CALL ujRekord( "LagziLajcsi", "123")
+
+8.
+
+DELIMITER $$
+CREATE PROCEDURE  modosit( IN nev VARCHAR(50))
+BEGIN
+UPDATE vevok SET vevonev = nev WHERE vevonev = "Kuka"; 
+END $$
+DELIMITER ;
+
+CALL modosit( "ModositottKuka" )
+
+9.
+
+DELIMITER $$
+CREATE PROCEDURE torol()
+BEGIN
+DECLARE top INT DEFAULT 0;
+	SELECT MAX( rid ) INTO top FROM rendelesek;
+	DELETE FROM rendelesek WHERE rid = top;
+END $$
+DELIMITER ;
+
+call torol();
+
+
+
+
